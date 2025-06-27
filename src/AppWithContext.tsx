bad0347bafb59749364e0286a7bf1a46d3cd7ddb
Michal Gawik <gawlik.zator@o2.pl>
2025-06-27 09:02:10 +0200

    create file

---
+++ src/AppWithContext.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppWithContext = () => {
+	return <div>App Context</div>
+}
+
+export default AppWithContext
---
+++ src/TodoContext.tsx
@@ -0,0 +1,2 @@
+import { createContext, useContext type ReactNode, useState } from "react";
+import { type Item, type ItemWithNoId } from "./utils";
