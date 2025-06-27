f04e76a7e49555f1c349ab3cf562f4d569320c2d
Michal Gawik <gawlik.zator@o2.pl>
2025-06-27 05:53:31 +0200

    create CardItemProps

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { BsTrash2 } from 'react-icons/bs'
 
-const CardItem = () => {
+import { type Item } from '../utils'
+
+type CardItemProps = Item & {
+	onDelete: (id: string) => void
+}
+
+const CardItem = ({
+	id,
+	title,
+	description,
+	category,
+	onDlete,
+}: CardItemProps) => {
 	return <div></div>
 }
 
