6e507c58b1478616ac6134eaa987681ecb095ed2
Michal Gawik <gawlik.zator@o2.pl>
2025-06-27 09:30:19 +0200

    add context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import AppWithContext from './AppWithContext.tsx'
+import { TodoProvider } from './TodoContext.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<TodoProvider>
+			<AppWithContext />
+		</TodoProvider>
+		{/* <App /> */}
+	</StrictMode>
 )
